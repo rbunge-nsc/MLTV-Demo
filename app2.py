4883d80fe3aa529d8b930e03acc1542922d63349
rbunge-nsc <52756750+rbunge-nsc@users.noreply.github.com>
2020-06-10 07:38:30 -0700

    Delete app2.py

--- app2.py
+++
@@ -1,9 +0,0 @@
-class app(object):
-
-    def __init__(self, environ, start_fn):
-        self.environ = environ
-        self.start_fn = start_fn
-
-    def __iter__(self):
-        self.start_fn('200 OK', [('Content-Type', 'text/plain')])
-        yield "Hello Again World!\n"
